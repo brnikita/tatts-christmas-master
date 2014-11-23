0bf5a04eed9438a369af4185d1420efef85bcfa3
branikita <nikita-bragin@mail.ru>
2014-11-24 00:06:53 +0300

    Start refactoring

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-    var f = {x: 0, y: 0},
+    var mousePosition = {x: 0, y: 0},
         playerObject = {
             title: 'Mr.',
             fname: '',
@@ -17,18 +17,160 @@
             password: ''
         },
         gameType = null,
-        plays_remain;
+        playsRemain,
+        $boardImage;
 
-    if (localStorage.getItem('_playerObject_') !== null) {
-        playerObject = JSON.parse(localStorage.getItem('_playerObject_'));
+    /**
+     * Function validates playerObject (data from registration form)
+     *
+     * @function
+     * @name checkValid
+     * @returns {undefined}
+     */
+    function checkValid() {
+        var player;
+
+        for (player in playerObject) {
+            if (playerObject.hasOwnProperty(player)) {
+                $('[name="' + player + '"]').val(playerObject[player]);
+            }
+        }
+
+        $('[data-valid]').each(function () {
+            var $this = $(this),
+                check = $this.attr('data-valid').split(','),
+                ret = true,
+                fieldName;
+
+            for (fieldName in check) {
+                if (check.hasOwnProperty(fieldName)) {
+                    if (check[fieldName] === 'email') {
+                        if (playerObject.email.indexOf('@') < 0)
+                            ret = false;
+                    }
+                    if (check[fieldName] === 'fname') {
+                        if (playerObject.fname.length < 1)
+                            ret = false;
+                    }
+                    if (check[fieldName] === 'password') {
+                        if (playerObject.password.length < 1)
+                            ret = false;
+                    }
+                    if (check[fieldName] === 'lname') {
+                        if (playerObject.lname.length < 1)
+                            ret = false;
+                    }
+                    if (check[fieldName] === 'mobile') {
+                        if (!playerObject.mobile.match(/^\d{10}$/))
+                            ret = false;
+                    }
+                    if (check[fieldName] === 'postcode') {
+                        if (parseInt(playerObject.postcode).toString().length < 3)
+                            ret = false;
+                    }
+                    if (check[fieldName] === 'sys') {
+                        if (playerObject.sys.length != 10)
+                            ret = false;
+                    }
+                }
+            }
+            if (!ret) {
+                $this.addClass('disabled');
+            } else {
+                $this.removeClass('disabled');
+            }
+        });
     }
 
-    playerObject.sys = '';
-    playerObject.password = '';
+    /**
+     * Function handles clicks on game area
+     *
+     * @function
+     * @name gameClickHandler
+     * @returns {boolean}
+     */
+    function gameClickHandler() {
+        var zoomTo,
+            game;
 
+        console.log('clicked panel');
+
+        if ($(this).attr('data-revealed') !== 'false') {
+            console.log('blocked');
+            return true;
+        }
+
+        $boardImage = $('#boardImage img.playImage');
+        zoomTo = $(this).attr('data-zoom').split(',');
+        game = $(this).attr('data-game').split(',');
+        mousePosition = {
+            x: parseFloat(zoomTo[0]),
+            y: parseFloat(zoomTo[1])
+        };
+        console.log(mousePosition);
+        $('.gameClick').addClass('hide');
+
+        // console.log('start zoom animation');
+        ui.blockPointer(200);
+
+        $boardImage.velocity({
+            scale: [7.5, 1],
+            translateZ: [1, 1],
+            translateX: [zoomTo[0] + '%', '0%'],
+            translateY: [zoomTo[1] + '%', '0%']
+        }, {
+            duration: 700, mobileHA: false, complete: function () {
+                $('#scratcher1').removeClass('no-event');
+                $('#boardImage').addClass('zoomed');
+            }
+        });
+
+        buildScratcher('#scratcher1', 'assets/images/panels/Panel_' + game + '.svg', '200', '#scratchRevealer',
+            function () {
+                var $scratchDone = $('#scratchDone');
+
+                $('#scratchRevealer').css({'background-image': 'none'});
+                $.post(config.api.scratch, {
+                    panel: game,
+                    gametype: gameType,
+                    playerObject: playerObject,
+                    screenid: ui.currentBoard
+                }, function (data) {
+                    if (data.message !== null) {
+                        alert(data.message);
+                    }
+
+                    playsRemain = data.plays_remain;
+                    $('.plays_remain').text(playsRemain);
+                    $scratchDone.removeAttr('data-board-swap').attr('data-event', 'unzoomBoard');
+
+                    if (data.game_state === true) {
+                        $scratchDone.attr('data-board-swap', '#winBoard');
+                    }
+
+                    if ((data.game_state === false) && (parseInt(playsRemain) === 0)) {
+                        $scratchDone.attr('data-board-swap', '#lossBoard');
+                    }
+
+                    if ((data.game_state === false) && (parseInt(playsRemain) !== 0)) {
+                        $('#tryAgain').find('[data-event="enterGame"]').attr('data-playfor', gameType);
+                        $scratchDone.attr('data-board-swap', '#tryAgain');
+                    }
+
+                    $('#scratchRevealer').css({'background-image': 'url(' + data.reveal + ')'});
+                    $('.gameClick[data-game="' + game + '"]')
+                        .css({'background-image': 'url(' + data.reveal + ')'})
+                        .attr('data-revealed', 'true');
+                }, 'json');
+            }
+        );
+    }
+
+    /**
+     * Handler of application load
+     */
     ui.events.on('uiloaded', function () {
-        var $boardImage;
-
+        console.log('uiloaded');
         IntroAnimation();
 
         //$.get('termsNSW.html', function (d) {
@@ -38,80 +180,20 @@
         //    $('#termsQLD').html(d);
         //});
 
-        $boardImage = $('#boardImage img.playImage');
+        if (localStorage.getItem('_playerObject_') !== null) {
+            playerObject = JSON.parse(localStorage.getItem('_playerObject_'));
+        }
 
-        $('.gameClick').on('mouseup', function () {
-            console.log('clicked panel');
+        playerObject.sys = '';
+        playerObject.password = '';
 
-            if ($(this).attr('data-revealed') != 'false') {
-                console.log('blocked');
-                return true;
-            }
-
-            var zoomto = $(this).attr('data-zoom').split(',');
-            var game = $(this).attr('data-game').split(',');
-
-            f = {
-                x: parseFloat(zoomto[0]),
-                y: parseFloat(zoomto[1])
-            };
-            console.log(f);
-            $('.gameClick').addClass('hide');
-
-            // console.log('start zoom animation');
-            ui.blockPointer(200);
-
-            $boardImage.velocity({
-                scale: [7.5, 1],
-                translateZ: [1, 1],
-                translateX: [zoomto[0] + '%', '0%'],
-                translateY: [zoomto[1] + '%', '0%']
-            }, {
-                duration: 700, mobileHA: false, complete: function () {
-                    $('#scratcher1').removeClass('no-event');
-                    $('#boardImage').addClass('zoomed');
-                }
-            });
-
-            // console.log('Build scratch Panel');
-            buildScratcher('#scratcher1', 'assets/images/panels/Panel_' + game + '.svg', '200', '#scratchRevealer',
-                function () {
-                    $('#scratchRevealer').css({'background-image': 'none'});
-                    $.post(config.api.scratch, {
-                        panel: game,
-                        gametype: gameType,
-                        playerObject: playerObject,
-                        screenid: ui.currentBoard
-                    }, function (data) {
-                        if (data.message !== null) alert(data.message);
-                        plays_remain = data.plays_remain;
-                        $('.plays_remain').text(plays_remain);
-                        $('#scratchDone').removeAttr('data-board-swap').attr('data-event', 'unzoomBoard');
-                        if (data.game_state === true) {
-                            $('#scratchDone').attr('data-board-swap', '#winBoard');
-                        }
-                        if ((data.game_state === false) && (parseInt(plays_remain) === 0)) {
-                            $('#scratchDone').attr('data-board-swap', '#lossBoard');
-                        }
-                        if ((data.game_state === false) && (parseInt(plays_remain) !== 0)) {
-                            $('#tryAgain').find('[data-event="enterGame"]').attr('data-playfor', gameType);
-                            $('#scratchDone').attr('data-board-swap', '#tryAgain');
-                        }
-
-                        $('#scratchRevealer').css({'background-image': 'url(' + data.reveal + ')'});
-                        $('.gameClick[data-game="' + game + '"]')
-                            .css({'background-image': 'url(' + data.reveal + ')'})
-                            .attr('data-revealed', 'true');
-                    }, 'json');
-                }
-            );
-        });
+        $('.gameClick').on('mouseup', gameClickHandler);
 
         $('input,select').on('change', function () {
-            var p = $(this).attr('name'),
-                v = $(this).val();
+            var $this = $(this),
+                fieldName = $this.attr('name');
 
-            playerObject[p] = v;
+            playerObject[fieldName] = $this.val();
 
             try {
                 localStorage.setItem('_playerObject_', JSON.stringify(playerObject));
@@ -122,26 +204,16 @@
         });
 
         checkValid();
-
     });
 
-
-// set fields from player playerObject
-
-
-// event binding for game panel
-
-    ui.events.on('enterGame', function (e, ret) {
+    /**
+     * Handler of click event on button 'Play for $250'
+     */
+    ui.events.on('enterGame', function (event, ret) {
         $('#scratchDone').removeAttr('data-board-swap');
 
-        console.log('entering game');
-
         ui.showLoad(true);
-        setTimeout(function () {
-            buildScratcher('#scratcher1', 'assets/images/panels/Panel_1.png', '200', '#scratchRevealer', function () {
-            });
-        }, 400);
-
+        buildScratcher('#scratcher1', '/assets/images/panels/Panel_1.png', '200', '#scratchRevealer');
         gameType = $(ret.element).attr('data-playfor');
 
         $.post(config.api.gameboard, {
@@ -149,18 +221,26 @@
             gametype: gameType,
             screenid: ui.currentBoard
         }, function (gamedata) {
+            var panels,
+                panelName;
+
             if (gamedata.message !== null) {
                 alert(gamedata.message);
                 return false;
             }
+
             ui.swapBoard('#game-board');
-            var panels = gamedata.panels;
+            panels = gamedata.panels;
             $('#gametitle').text(gamedata.gametitle);
-            for (var n in panels) {
-                $('.gameClick[data-game="' + panels[n].panel_number + '"]')
-                    .css({'background-image': 'url(' + panels[n].image + ')'})
-                    .attr('data-revealed', panels[n].isRevealed);
+
+            for (panelName in panels) {
+                if (panels.hasOwnProperty(panelName)) {
+                    $('.gameClick[data-game="' + panels[panelName].panel_number + '"]')
+                        .css({'background-image': 'url(' + panels[panelName].image + ')'})
+                        .attr('data-revealed', panels[panelName].isRevealed);
+                }
             }
+
         }, 'json').always(function (e, s, d) {
             console.log(d);
             ui.showLoad(false);
@@ -169,7 +249,7 @@
 
     ui.events.on('tattsJoin', function () {
         $.post(config.api.register, {playerObject: playerObject, screenid: ui.currentBoard}, function (join) {
-            if (join.registered == true) {
+            if (join.registered === true) {
                 ui.swapBoard('#member-play');
             } else {
                 alert(join.message);
@@ -183,7 +263,10 @@
             screenid: ui.currentBoard,
             gametype: gameType
         }, function (claim) {
-            if (claim.message !== null) alert(claim.message);
+            if (claim.message !== null) {
+                alert(claim.message);
+            }
+
             if (claim.claim == true) {
                 alert('You will now receive an email with instructions on how to complete your prize claim.');
                 ui.swapBoard('#firstBoard');
@@ -191,10 +274,16 @@
         }, 'json');
     });
 
-    ui.events.on('memberLogin', function (e, elem) {
-        $.post(config.api.member_auth, {playerObject: playerObject, screenid: ui.currentBoard}, function (auth) {
-            console.log(auth);
-            if (auth.auth == true) {
+    /**
+     * Handler of user login
+     */
+    ui.events.on('memberLogin', function () {
+        ui.swapBoard('#member-play');
+        $.post(config.api.member_auth, {
+            playerObject: playerObject,
+            screenid: ui.currentBoard
+        }, function (auth) {
+            if (auth.auth === true) {
                 ui.swapBoard('#member-play');
             } else {
                 alert('Member not found');
@@ -202,9 +291,10 @@
         }, 'json');
     });
 
-    ui.events.on('unzoomBoard', function () {
+    ui.events.on('unzoomBoard', function (event) {
         $('#boardImage').removeClass('zoomed');
         $('#scratcher1').addClass('no-event');
+
         $boardImage.velocity({
             translateZ: 1,
             translateX: '0.00001%',
@@ -217,73 +307,27 @@
         });
     });
 
-    ui.events.on('termsNSW', function (e, elem) {
+    ui.events.on('termsNSW', function (event, element) {
         $('#termsNSW').hide();
         $('#termsQLD').hide();
         $('#termsNSW').show(); //here
 
         $('.terms-swap').removeClass('gold-text');
-        $(elem.element).addClass('gold-text');
+        $(element.element).addClass('gold-text');
     });
 
-    ui.events.on('termsQLD', function (e, elem) {
+    ui.events.on('termsQLD', function (event, element) {
         $('#termsNSW').hide();
         $('#termsQLD').hide();
         $('#termsQLD').show(); //here
 
         $('.terms-swap').removeClass('gold-text');
-        $(elem.element).addClass('gold-text');
+        $(element.element).addClass('gold-text');
     });
 
-
-    function checkValid() {
-
-        for (var p in playerObject) {
-            $('[name="' + p + '"]').val(playerObject[p]);
-        }
-
-        $('[data-valid]').each(function () {
-            var check = $(this).attr('data-valid').split(',');
-            var ret = true;
-            for (var v in check) {
-                if (check[v] == 'email') {
-                    if (playerObject.email.indexOf('@') < 0)
-                        ret = false;
-                }
-                if (check[v] == 'fname') {
-                    if (playerObject.fname.length < 1)
-                        ret = false;
-                }
-                if (check[v] == 'password') {
-                    if (playerObject.password.length < 1)
-                        ret = false;
-                }
-                if (check[v] == 'lname') {
-                    if (playerObject.lname.length < 1)
-                        ret = false;
-                }
-                if (check[v] == 'mobile') {
-                    if (!playerObject.mobile.match(/^\d{10}$/))
-                        ret = false;
-                }
-                if (check[v] == 'postcode') {
-                    if (parseInt(playerObject.postcode).toString().length < 3)
-                        ret = false;
-                }
-                if (check[v] == 'sys') {
-                    if (playerObject.sys.length != 10)
-                        ret = false;
-                }
-            }
-            if (!ret) {
-                $(this).addClass('disabled');
-            } else {
-                $(this).removeClass('disabled');
-            }
-        });
-    }
-
     function IntroAnimation() {
+        var $howToPlayBox = $('.how-to-play-box .hand');
+
         //init intro animation
         $('#firstBoard .how-to-play-box .rotator').cycle({
             fx: 'scrollHorz',
@@ -316,37 +360,37 @@
                 if (options.currSlide == 4) {
                     //hand movement
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({opacity: 1, left: '+=15%', bottom: '+=12%'}, 400);
+                        $howToPlayBox.animate({opacity: 1, left: '+=15%', bottom: '+=12%'}, 400);
                     }, 0);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '+=15%', bottom: '+=25%'}, 400);
+                        $howToPlayBox.animate({left: '+=15%', bottom: '+=25%'}, 400);
                     }, 400);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '-=4%', bottom: '-=25%'}, 400);
+                        $howToPlayBox.animate({left: '-=4%', bottom: '-=25%'}, 400);
                     }, 800);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '+=15%', bottom: '+=25%'}, 400);
+                        $howToPlayBox.animate({left: '+=15%', bottom: '+=25%'}, 400);
                     }, 1200);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '-=4%', bottom: '-=25%'}, 400);
+                        $howToPlayBox.animate({left: '-=4%', bottom: '-=25%'}, 400);
                     }, 1600);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '+=21%'}, 600);
+                        $howToPlayBox.animate({left: '+=21%'}, 600);
                     }, 2000);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '+=15%', bottom: '+=25%'}, 400);
+                        $howToPlayBox.animate({left: '+=15%', bottom: '+=25%'}, 400);
                     }, 2600);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '-=4%', bottom: '-=25%'}, 400);
+                        $howToPlayBox.animate({left: '-=4%', bottom: '-=25%'}, 400);
                     }, 3000);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '+=15%', bottom: '+=25%'}, 400);
+                        $howToPlayBox.animate({left: '+=15%', bottom: '+=25%'}, 400);
                     }, 3400);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({left: '-=4%', bottom: '-=25%'}, 400);
+                        $howToPlayBox.animate({left: '-=4%', bottom: '-=25%'}, 400);
                     }, 3800);
                     setTimeout(function () {
-                        $('.how-to-play-box .hand').animate({opacity: 0, left: '+=13%', bottom: '-=12%'}, 400);
+                        $howToPlayBox.animate({opacity: 0, left: '+=13%', bottom: '-=12%'}, 400);
                     }, 4400);
 
                     //scratch off boxes
